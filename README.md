c501274b6a72602bcbf69b9bb5f2667769c27ef8
BillGreen <1098057724@qq.com>
2019-11-08 12:00:16 +0800

    1. 更新基础库

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 3djs
+# 3d
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
-    <div id="app">
-        <h1>部署测试</h1>
-    </div>
+  <div id="app">
+    <transition name="slide-fade">
+      <router-view></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
-
-    export default {
-        name: 'app',
-        data() {
-            return {
-                options: [
-                    {
-                        label: 'gong'
-                    }
-                ]
-            }
-        },
-        methods: {}
-    }
+// import Three from ''
+export default {
+  name: "app"
+};
 </script>
 
 <style>
+/* 可以设置不同的进入和离开动画 */
+/* 设置持续时间和动画函数 */
+.slide-fade-enter-active {
+  transition: all 0.1s ease;
+}
 
-    .tree {
+.slide-fade-leave-active {
+  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
+}
 
-    }
+.slide-fade-enter, .slide-fade-leave-to
+        /* .slide-fade-leave-active for below version 2.1.8 */
+ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
