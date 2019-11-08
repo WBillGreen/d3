fc007511e3e79a003449b398cd3aa52b4ccccb61
BillGreen <1098057724@qq.com>
2019-11-08 11:06:22 +0800

    1.初始化项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'app',
+        data(){
+            return{
+               options:[
+                   {
+                       label:'gong'
+                   }
+               ]
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style>
+
+    .tree{
+
+    }
+</style>
