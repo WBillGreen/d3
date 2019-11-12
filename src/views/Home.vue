65dc6ba6666b4ce98d14ef787e7de845af44bed5
BillGreen <1098057724@qq.com>
2019-11-12 12:18:38 +0800

    no message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 // import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import moon from "../assets/bbb.jpg";
+
 let stats, renderer, scene, camera, directionalLight;
 export default {
   name: "home",
@@ -42,7 +43,7 @@ export default {
         1,
         10000
       );
-      camera.position.z = 1000;
+      camera.position.z = 10000;
     },
     initLight() {
       directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
@@ -56,23 +57,75 @@ export default {
       scene.add(circle);
     },
     initCone() {
-      var loader = new THREE.TextureLoader();
+      let loader = new THREE.TextureLoader();
+      let group = new THREE.Group();
       /*材质 纹理加载器*/
       loader.load(
         moon,
         texture => {
-          window.console.log(texture);
-          let geometry = new THREE.BoxBufferGeometry(100, 10, 100, 1, 2, 2);
-          let material = new THREE.MeshBasicMaterial({ map: texture });
-          let circle = new THREE.Mesh(geometry, material);
-          scene.add(circle);
+          for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+              let geometry = new THREE.BoxBufferGeometry(100, 10, 100, 1, 2, 2);
+              let material = new THREE.MeshBasicMaterial({ map: texture });
+              let circle = new THREE.Mesh(geometry, material);
+              circle.position.set(i * 100, 0, j * 100);
+              group.add(circle);
+            }
+          }
+
+          for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+              let geometry = new THREE.BoxBufferGeometry(10, 100, 100, 1, 2, 2);
+              let material = new THREE.MeshBasicMaterial({ map: texture });
+              let circle = new THREE.Mesh(geometry, material);
+              circle.position.set(-50, 100 * i + 50, j * 100);
+              group.add(circle);
+            }
+          }
+          for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+              let geometry = new THREE.BoxBufferGeometry(10, 100, 100, 1, 2, 2);
+              let material = new THREE.MeshBasicMaterial({ map: texture });
+              let circle = new THREE.Mesh(geometry, material);
+              circle.position.set(1000 - 50, 100 * i + 50, j * 100);
+
+              group.add(circle);
+            }
+          }
+          for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+              let geometry = new THREE.BoxBufferGeometry(10, 100, 100, 1, 2, 2);
+              let material = new THREE.MeshBasicMaterial({ map: texture });
+              let circle = new THREE.Mesh(geometry, material);
+              circle.position.set(
+                1200 - i * Math.sqrt((100 * 100) / 2),
+                Math.sqrt((100 * 100) / 2) * i +
+                  (1100 - Math.sqrt((490 * 490) / 2)),
+                j * 100
+              );
+              circle.rotation.z = -15;
+              group.add(circle);
+            }
+          }
+          for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+              let geometry = new THREE.BoxBufferGeometry(10, 100, 100, 1, 2, 2);
+              let material = new THREE.MeshBasicMaterial({ map: texture });
+              let circle = new THREE.Mesh(geometry, material);
+              circle.position.set(
+                -300 + i * Math.sqrt((100 * 100) / 2),
+                Math.sqrt((100 * 100) / 2) * i +
+                  (1100 - Math.sqrt((490 * 490) / 2)),
+                j * 100
+              );
+              circle.rotation.z = 15;
+              group.add(circle);
+            }
+          }
+          scene.add(group);
         },
-        xhr => {
-          window.console.log(xhr);
-        },
-        err => {
-          window.console.log(err);
-        }
+        xhr => {},
+        err => {}
       );
     },
     render() {
